97716789a3b93a2317f8d914ea1e33190fe7cf1e
Alfred Padawangi <alfredp77@outlook.com>
2019-04-17 16:36:46 +0800

    check_bus_coming

--- buspy/datamall_query.py
+++ buspy/datamall_query.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 base_url = 'http://datamall2.mytransport.sg/ltaodataservice/'
 rel_path="../buspy/tokens.json"
@@ -10,8 +11,7 @@ def parse_arrival_data(res_json):
     arrival = res_json["Services"][0]["NextBus"]["EstimatedArrival"]
     return arrival
 
-def get_arrival_time(bus_stop_code, service_no):
-    
+def get_arrival_time(bus_stop_code, service_no):    
     headers = {
         "AccountKey": data["lta"],
         "accept":"application/json"       
@@ -21,4 +21,7 @@ def get_arrival_time(bus_stop_code, service_no):
     resp = requests.get(url , headers= headers )
     return parse_arrival_data(resp.json())
 
-print(get_arrival_time(67379, 372))
+def check_bus_coming(bus_stop_code, service_no, requested_time):
+    # get arrival time
+    # if arrival time is within requested_time+-10mins, then return a message
+    return ""
--- tests/datamall_query_test.py
+++ tests/datamall_query_test.py
@@ -1,6 +1,8 @@
 import unittest
 import buspy.datamall_query as query
 import json
+from unittest.mock import MagicMock
+import datetime
 
 class DataMallQueryTests(unittest.TestCase):
     def test_should_be_able_to_parse_arrival_time(self):
@@ -9,6 +11,20 @@ class DataMallQueryTests(unittest.TestCase):
         arrival = query.parse_arrival_data(data)
         self.assertEqual("2019-04-17T13:37:43+08:00", arrival)
         
+    def test_magicmock(self):
+        query.get_arrival_time = MagicMock(return_value="a mock")
+        self.assertEqual("a mock", query.get_arrival_time(67379,372))
 
+    def test_returns_no_bus_coming_when_bus_is_still_far(self):
+        requested_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
+        arrival_time = datetime.datetime.now() - datetime.timedelta(minutes=12)
+        query.get_arrival_time = MagicMock(return_value=arrival_time.isoformat())
+        result = query.check_bus_coming(67379, 372, requested_time.isoformat)
+        self.assertEqual("", result)
+
+    # def test_returns_bus_is_coming_when_its_within_10_mins(self):
+    #     query.get_arrival_time = MagicMock(return_value="2019-04-17T13:37:43+08:00")
+    #     result = query.check_bus_coming(67379, 372, "some time string")
+    #     self.assertEqual("Bus is coming soon", result)
 if __name__ == '__main__':
     unittest.main()
